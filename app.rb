b16b46dab58665568e431fd848a866d47e686ae7
Rob Holloway <robbiechicago@gmail.com>
2015-09-02 20:26:17 +0100

    basic calculator working, although when the page re-renders it forgets the h1 bit.  Weird.

--- app.rb
+++ app.rb
@@ -1,5 +1,21 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'pry'
+
+post '/calculate' do
+  @num1 = params[:num1].to_i
+  @num2 = params[:num2].to_i
+  # binding.pry
+  case params[:op]
+  when 'plus' then @result = @num1 + @num2
+  when 'minus' then @result = @num1 - @num2
+  when 'divide' then @result = @num1 / @num2
+  when 'multiply' then @result = @num1 * @num2
+  end
+  erb :basic
+end
+
+
 
 get '/' do
   @title = 'Basic'
